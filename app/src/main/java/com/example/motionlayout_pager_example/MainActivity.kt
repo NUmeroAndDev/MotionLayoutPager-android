c197eaffe61862eb0b45873c567ad002c5884f5f
NUmeroAndDev <numero.dev.and@gmail.com>
2019-12-20 11:25:36 +0900

    Implemented MotionLayout pager

--- app/src/main/java/com/example/motionlayout_pager_example/MainActivity.kt
+++ app/src/main/java/com/example/motionlayout_pager_example/MainActivity.kt
@@ -5,15 +5,23 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.constraintlayout.motion.widget.MotionLayout
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var currentPosition = 0
+    private val itemList = listOf(
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        setupMotion()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -25,6 +33,59 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setupMotion() {
+        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) {
+            }
+
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startedId: Int, endId: Int) {
+            }
+
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                when (currentId) {
+                    R.id.move_left -> {
+                        if (currentPosition > 0) {
+                            currentPosition--
+                        } else {
+                            currentPosition = itemList.lastIndex
+                        }
+                        motionLayout?.progress = 0F
+                        updateView()
+                    }
+                    R.id.move_right -> {
+                        if (currentPosition < itemList.lastIndex) {
+                            currentPosition++
+                        } else {
+                            currentPosition = 0
+                        }
+                        motionLayout?.progress = 0F
+                        updateView()
+                    }
+                }
+            }
+        })
+        updateView()
+    }
+
+    private fun updateView() {
+        centerView.text = "${itemList[currentPosition]}"
+
+        rightView.text = if (currentPosition == itemList.lastIndex) {
+            "${itemList.first()}"
+        } else {
+            "${itemList[currentPosition + 1]}"
+        }
+
+        leftView.text = if (currentPosition == 0) {
+            "${itemList.last()}"
+        } else {
+            "${itemList[currentPosition - 1]}"
         }
     }
 }
